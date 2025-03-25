9ec3dc725f8f57f296c0b76ad5d6056f14ab0df3
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-03-24 23:12:03 -0300

    classe

---
+++ class/class.js
@@ -0,0 +1,14 @@
+let animais = {
+     cachorro: 'pit-bul',
+     cacacteriticar(){
+        console.log('farejador policial');
+     }
+}
+
+
+let novoANIMAL = Object.create(animais);
+
+novoANIMAL.cachorro = 'labrador';
+
+console.log(novoANIMAL);
+novoANIMAL.cacacteriticar();
