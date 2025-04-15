534965928055d219aa9f7205e8b4a8971894495b
Lindrielton silva <161902702+lindrielton@users.noreply.github.com>
2025-04-14 21:17:32 -0300

    bliblioteca

--- 24-sistema-de-voo/class.js
+++ 24-sistema-de-voo/class.js
@@ -51,5 +51,24 @@ console.log(novovoo);
 novovoo.consultarAssentosDisponiveis();
 
 
+/*Crie uma classe chamada "Livro" que representa um livro em um sistema de biblioteca.
+
+A classe deve ter os seguintes atributos:
+
+titulo: representando o título do livro (string).
+
+autor: representando o autor do livro (string).
+
+disponivel: representando a disponibilidade do livro (boolean).
+
+A classe deve ter os seguintes métodos:
+
+emprestar(): verifica se o livro está disponível para empréstimo e, se estiver, atualiza a disponibilidade para false.
+
+devolver(): verifica se o livro não está disponível para empréstimo e, se não estiver, atualiza a disponibilidade para true.
+
+consultarDisponibilidade(): r
+
+
 
 
